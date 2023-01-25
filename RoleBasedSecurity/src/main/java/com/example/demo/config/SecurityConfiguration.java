073c89e96cbc6cd2f1adadec46a46db82012ea08
EgERohit2 <118536484+EgERohit2@users.noreply.github.com>
2023-01-25 17:56:29 +0530

    Add files via upload

---
+++ RoleBasedSecurity/src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableAutoConfiguration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+	@Bean
+	AuthenticationProvider authentationProvider() {
+		DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(new BCryptPasswordEncoder());
+		return provider;
+		
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/")
+		.permitAll()
+		.antMatchers("/home")
+		.hasAuthority("USER");
+		
+		
+		http.authorizeRequests()
+		.antMatchers("/admin")
+		.hasAuthority("ADMIN")
+		.anyRequest()
+		.authenticated()
+		.and()
+		.httpBasic();
+	}
+}
